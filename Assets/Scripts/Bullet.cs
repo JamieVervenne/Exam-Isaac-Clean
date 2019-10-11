2c67a42a5f4d762f3d68e9e64b0eb7f671e48862
JamieVervenne <46885955+JamieVervenne@users.noreply.github.com>
2019-10-11 08:45:04 +0200

    Assets

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float speed;
+    public float lifetime;
+    public int damage;
+
+    private GameObject triggerEnemy;
+
+    void Update()
+    {
+        transform.Translate(Vector2.up * Time.deltaTime * speed);
+
+        lifetime -= 1 * Time.deltaTime;
+
+        if (lifetime <= 0)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        if(collision.collider.tag == "Wall")
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Hazard")
+        {
+            triggerEnemy = other.gameObject;
+            triggerEnemy.GetComponent<Thwomp>().health -= damage;
+            Destroy(this.gameObject);
+        }
+        if (other.tag == "WallShooter")
+        {
+            triggerEnemy = other.gameObject;
+            triggerEnemy.GetComponent<WallShooter>().health -= damage;
+            Destroy(this.gameObject);
+        }
+        if (other.tag == "Player")
+        {
+            triggerEnemy = other.gameObject;
+            triggerEnemy.GetComponent<PlayerControl>().health -= damage;
+            Destroy(this.gameObject);
+        }
+    }
+}
